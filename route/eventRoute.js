3ad5e1e7908b41c5d95489c0bd7fc555ed746460
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-01-08 12:02:49 +0545

    authentication middleware added

--- route/eventRoute.js
+++ route/eventRoute.js
@@ -1,10 +1,11 @@
 
 const {createEvent } = require("../controller/teacher/teacherController")
+const isAuthenticated = require("../middleware/isAuthenticated")
 const catchAsync = require("../services/catchAsync")
 
 const router = require("express").Router()
 
 router.route("/")
-    .post(catchAsync(createEvent))
+    .post(isAuthenticated,catchAsync(createEvent))
 
 module.exports = router
