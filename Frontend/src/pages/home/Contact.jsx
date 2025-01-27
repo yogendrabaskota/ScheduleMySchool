dc1e96637c2a8761860739d66b232cb6accee2cf
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-01-27 16:00:20 +0545

    endpoint update

--- Frontend/src/pages/home/Contact.jsx
+++ Frontend/src/pages/home/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
     e.preventDefault(); // Prevent form reload
 
     try {
-      const response = await fetch('http://localhost:5000/api/user/data', {
+      const response = await fetch('https://schedulemyschool.onrender.com/api/user/data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
